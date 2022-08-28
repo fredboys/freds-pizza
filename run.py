9c83446a0e27ed9c27fd49f132937b237849a202
fredboys <freddieboys@hotmail.co.uk>
2022-08-28 09:27:02 +0000

    Add welcome message and user input to place order

--- run.py
+++ run.py
@@ -9,23 +9,47 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_files('creds.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET =GSPREAD_CLIENT.OPEN('freds-pizza')
+SHEET = GSPREAD_CLIENT.open('freds-pizza')
 
 pizza_menu = {
-    "1": "Margherita"
-    "2": "BBQ Chicken"
-    "3": "Pepperoni"
-    "4": "Hawaiian"
-    "5": "Veggie"
-    "6": "Meat Lover"
+    "1": "Margherita",
+    "2": "BBQ Chicken",
+    "3": "Pepperoni",
+    "4": "Hawaiian",
+    "5": "Veggie",
+    "6": "Meat Lover",
 }
 
 size_price = {
-    "1": "Small - 9 INCH - £5"
-    "2": "Medium - 11 INCH - £8"
-    "3": "Large - 13 INCH - £12"
+    "1": "Small - 9 INCH - £5",
+    "2": "Medium - 11 INCH - £8",
+    "3": "Large - 13 INCH - £12",
 }
 
+def welcome():
+    """
+    Function to welcome the customer or 
+    leave if they dont want to order
+    """
+    while True:
+        print("Hola, Welcome to Fred's Pizzas!")
+        print("Would you like to place and order? [Y]es or [N]o")
+        user_choice = input("Enter :")
+        user_choice = user_choice.strip()
+        if(user_choice == "Y" or user_choice == "y"):
+            print("Lets get you the menu...\n")
+            for index, pizza in pizza_menu.items():
+                    print(index, pizza)
+            break
+        elif(user_choice == "N" or user_choice == "n"):
+                print("Hopefully see you next time!")
+                sys.exit()
+        else:
+            print("That not quite right")
+            print("Make sure you either enetered Y or N\n")
+            return welcome()
+
+welcome()
