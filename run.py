0788908a9867fc55e4c8cf5fe3444852b5dcf758
fredboys <freddieboys@hotmail.co.uk>
2022-08-28 09:03:00 +0000

    Add pizza menu and size cost list to run.py

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import sys
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -11,3 +13,19 @@ CREDS = Credentials.from_service_account_files('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET =GSPREAD_CLIENT.OPEN('freds-pizza')
+
+pizza_menu = {
+    "1": "Margherita"
+    "2": "BBQ Chicken"
+    "3": "Pepperoni"
+    "4": "Hawaiian"
+    "5": "Veggie"
+    "6": "Meat Lover"
+}
+
+size_price = {
+    "1": "Small - 9 INCH - £5"
+    "2": "Medium - 11 INCH - £8"
+    "3": "Large - 13 INCH - £12"
+}
+
