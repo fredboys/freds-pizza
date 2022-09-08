fa3b25263ea32820eb00b12a5822bcb17b0cf24e
fredboys <freddieboys@hotmail.co.uk>
2022-09-08 21:15:52 +0000

    Fix typo in add_dip funcion

--- run.py
+++ run.py
@@ -194,7 +194,7 @@ def add_dip():
             sys.exit()
             break
         else:
-            print("\nThat not quite right")
+            print("\nThat's not quite right")
             print("Make sure you either entered Y or N\n")
 
     return user_dip_input
